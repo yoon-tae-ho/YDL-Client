42a6aad4df70e8f49d3591d22ae90b16e761e0d5
yoontaeho <pln0302@yonsei.ac.kr>
2022-02-15 15:14:40 +0900

    Fetched lectures data and stored it in state

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Browse from "./routes/Browse";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/browse" element={<Browse />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
