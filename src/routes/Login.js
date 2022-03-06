9996ccf3237023a99b43598726e55b406ddcd2f0
yoontaeho <pln0302@yonsei.ac.kr>
2022-03-06 15:17:28 +0900

    Added kakao oauth

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -5,9 +5,17 @@ const Login = () => {
   return (
     <div style={{ marginTop: "100px" }}>
       <Header />
-      <a href={`${process.env.REACT_APP_API_URL}/user/social/github/start`}>
-        Login with Github
-      </a>
+      <div>
+        <a href={`${process.env.REACT_APP_API_URL}/user/social/github/start`}>
+          <i className="fab fa-github"></i>
+          Login with Github
+        </a>
+      </div>
+      <div>
+        <a href={`${process.env.REACT_APP_API_URL}/user/social/kakao/start`}>
+          Login with Kakao
+        </a>
+      </div>
     </div>
   );
 };
