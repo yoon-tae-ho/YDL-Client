436745c81e9d31fc23a83cd69bf31b68634fbc53
yoontaeho <pln0302@yonsei.ac.kr>
2022-04-09 16:37:52 +0900

    Added row slider loading animation

--- src/components/RowLoading.js
+++ src/components/RowLoading.js
@@ -1,6 +1,23 @@
 import React from "react";
 import styles from "../css/RowLoading.module.css";
 
+const getItems = (max) => {
+  const items = [];
+  for (let i = 0; i < max; ++i) {
+    items.push(
+      <div
+        className={styles.item}
+        key={`rowLoading_item_${i}`}
+        style={{
+          WebkitAnimationDelay: `${0.2 * i}s`,
+          animationDelay: `${0.2 * i}s`,
+        }}
+      ></div>
+    );
+  }
+  return items;
+};
+
 const RowLoading = ({ header }) => {
   return (
     <div className={styles.row}>
@@ -9,7 +26,7 @@ const RowLoading = ({ header }) => {
           <div className={styles.title}></div>
         </div>
       )}
-      <div className={styles.content}></div>
+      <div className={styles.content}>{getItems(7)}</div>
     </div>
   );
 };
