cb49bb7c7f1fd10655f632053a6b9e5476590852
yoontaeho <pln0302@yonsei.ac.kr>
2022-05-18 14:11:18 +0900

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.text}>
-        <span className={styles.text_block}>개발자: 윤태호, 손명현</span>
-        <span className={styles.text_block}>
-          이메일 주소: pln0302@yonsei.ac.kr, franksmh0409@naver.com
-        </span>
+        <span className={styles.text_block}>개발자: 윤태호</span>
+        <span className={styles.text_block}>이메일: pln0302@yonsei.ac.kr</span>
       </div>
     </footer>
   );
